8d0f5f65845d4b13aeb63fcdf980a13c7a8713f2
juri-chas <juri.semjonow@chasacademy.se>
2025-10-08 15:14:22 +0200

    localStorage och ny JS fil osv

---
+++ localStorage.js
@@ -0,0 +1,11 @@
+const savedName = localstorage.getItem("name");
+//om vi har fått in ett så vill vi visa upp den.
+if (savedName) {
+  nameTitle.textContent =`Ditt namn: ${savedName}`;
+}
+const savedStringArray - localStorage.getItem('test')
+const myArray = JSon.parse(savedStringArray)
+console.log(myArray)
+const saveName = () =>{
+  console.log("saveName");
+}
